1bce311368aa7ec159405920c67e26a35d8769dc
RafaelTS <rafaeltorress@gmail.com>
2025-06-13 13:51:08 -0300

    first commit

---
+++ cypress.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    baseUrl: 'https://erickwendel.github.io/vanilla-js-web-app-example',
+    testIsolation: false
+  },
+});
