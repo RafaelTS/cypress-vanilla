96bb648f0fefa1c0de4216348811d6e83610490c
RafaelTS <rafaeltorress@gmail.com>
2025-06-13 15:31:27 -0300

    finish test 1

--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -1,3 +1,5 @@
+import assert from 'assert'
+
 class RegisterForm {
   elements = {
     titleInput: () => cy.get ('#title'),
@@ -22,32 +24,52 @@ typeUrl(text) {
 }
 
 const registerForm = new RegisterForm()
+const colors = {
+  errors: 'rgb(220, 53, 69)'
+}
 
 describe('Image Registration', () => {
   describe('Submitting an image with invalid inputs', () => {
-  const input = {
-    title: '',
-    url:''
-  }
+    after(() => {
+      cy.clearAllLocalStorage()
+    })
+
+    const input = {
+      title: '',
+      url:''
+    }
   
-  it('Given I am on the image registration page', () => {
+    it('Given I am on the image registration page', () => {
     cy.visit('/')
     })
 
     it (`When I enter "${input.title}" in the title field`, () => {
       registerForm.typeTitle(input.title)
     })
+
     it (`Then I enter "" in the URL field`, () => {
       registerForm.typeUrl(input.url)
     })
+
     it (`Then I click the submit button`, () => {
       registerForm.clickSubmit()
     })
+
     it (`Then I should see "Please type a title for the image" message above the title field`, () => {
-      registerForm.elements.titleFeedback().should()
+      registerForm.elements.titleFeedback().should('contains.text', 'Please type a title for the image')
     })
-    it (`And I should see "Please type a valid URL" message above the imageUrl field`)
-    it (`And I should see an exclamation icon in the title and URL fields`)
-    
+
+    it (`And I should see "Please type a valid URL" message above the imageUrl field`, () => {
+      registerForm.elements.urlFeedback().should('contains.text', 'Please type a valid URL')
+    })
+
+    it (`And I should see an exclamation icon in the title and URL fields`, () => {
+
+      registerForm.elements.titleInput().should(([element]) => {
+        const styles = window.getComputedStyle(element)
+        const border = styles.getPropertyValue('border-right-color')
+        assert.strictEqual(border, colors.errors)
+      })
+    })    
   })
 })
